dd24b0a5c21f30bb40f02dc989d39b9de3a21ff4
jackphelps@gmail.com <jackphelps@gmail.com>
2014-03-02 16:48:37 -0500

    added bedford squarespace theme support and extra refinement and testing

--- overplay.js
+++ overplay.js
@@ -1,33 +1,84 @@
+//overplay JS
+//created by Jack Phelps
+
 YUI().use('node', function (Y) {
 
     //upload your files, set up your embed code, and get the div to be overlain
     var pageName        = 'video1';
     var nodeLocation    = '[data-url-id="' + pageName + '"].parallax-item .has-main-image'; // for squarespace marquee theme
+    //var nodeLocation    = '.sqs-active-slide'; //for squarespace bedford theme
     var vimeoURL        = 'https://player.vimeo.com/video/86632660';
-    var playButtonURL   = 'https://static.squarespace.com/static/52fe6465e4b0497565b34eca/t/53026e1ee4b0b0825b1a8a4c/1392668190314/play_button.png';
+    var vimeoMsgURL     = '*' //you should switch this to a specific embed target for security
+    var playButtonURL   = 'play_button.png';
+    //var playButtonURL   = 'https://static.squarespace.com/static/52ca173ce4b00d052b1f1e10/t/52fc4cf4e4b01b2bb4f80637/1392266485115/play_button.png';
     var playButtonSize  = '100';
     var playerColor     = 'FF00EE';
 
-    //suggested that you do not change this unless you know what you're doing
-    var embedCode       = '<iframe src="' + vimeoURL + '?title=0&amp;byline=0&amp;portrait=0&amp;color=' + playerColor + '&amp;autoplay=1" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-    var buttonCode      = '<div style="position:absolute;top:50%;left:50%;height:' + playButtonSize + 'px;width:' + playButtonSize + ';margin: -' + Math.round(playButtonSize / 2) + 'px 0 0 -' + Math.round(playButtonSize / 2) + 'px;opacity:0.8;"><img src="' + playButtonURL + '"></div>';
+    //embed code and play button styling -- maybe don't change this unless you know what you're doing
+    var embedCode       = '<iframe id="vimeo_iframe" src="' + vimeoURL + '?title=0&amp;byline=0&amp;portrait=0&amp;color=' + playerColor + '&amp;autoplay=1&amp;api=1&amp;player_id=overplay" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+    var buttonCode      = '<div style="height:' + playButtonSize + 'px;width:' + playButtonSize + 'px;position:absolute;padding:0;margin: -' + Math.round(playButtonSize / 2) + 'px 0 0 -' + Math.round(playButtonSize / 2) + 'px;top:50%;left:50%;opacity:0.8;"><img src="' + playButtonURL + '"></div>';
 
-    console.log('thingies loaded');
+    //declare the iframe and associated vars ahead of time because they don't exist yet but will need to be accessible out of creation scope
+    var videoNode,
+        iframe;
+    
+    //set up the click-to-play node & its callback
+    function addSwap(elem) {
+        elem.on("click", insertVideo);
+        elem.setStyle('cursor','pointer');
+        elem.insert(buttonCode);
+    }
+
     //func to insert iframe on button click
     var insertVideo = function(e) {
-        e.target.setHTML(embedCode);
+        console.log('clicked to play');
+        videoNode.setHTML(embedCode);
+        //now that the player exists, let's listen for events and set up its finish event listener
+        if (window.addEventListener) {
+            window.addEventListener('message', onMessageReceived, false);
+        } else {
+            window.addEventListener('message', onMessageReceived, false);
+        };
+        iframe = document.getElementById('vimeo_iframe');
+        var msg = {method: 'addEventListener'};
+        iframe.contentWindow.postMessage(JSON.stringify(msg), vimeoMsgURL);
+
     };
 
-    //set up the target node & click callback
-    function addSwap(elem) {
-        var targetNode = Y.one(elem);
-        targetNode.on("click", insertVideo);
-        targetNode.setStyle('cursor','pointer');
-        targetNode.prepend(buttonCode);
+    //get an event from vimeo player
+    function onMessageReceived(e) {
+        console.log('message received');
+        console.log(e);
+        var msg = JSON.parse(e.data);
+        console.log(msg.event);
+        switch (msg.event) {
+            case 'ready':
+                onReady();
+                break;
+            case 'finish': 
+                onFinish();
+                break;
+        };
+    };
+
+    //player loaded and ready
+    function onReady() {
+        var msg = {method: 'addEventListener', value: 'finish'};
+        iframe.contentWindow.postMessage(JSON.stringify(msg), vimeoMsgURL);
     }
+
+    //got finish event from vimeo player
+    //take some action, like redirecting somewhere
+    function onFinish() {
+        console.log('finished playing');
+        window.location = 'http://www.theplanaproj.com/';
+    }
+
+    //domready set stuff up
     Y.use('node', function() {
         Y.on('domready', function() {
-            addSwap(nodeLocation);
+            videoNode = Y.one(nodeLocation);
+            addSwap(videoNode);
         });
     });
 });
--- readme.md
+++ readme.md
@@ -4,15 +4,10 @@ As in, it plays over something and it also does something when the playing is ov
 
 Requires: YUI3 & Vimeo, tested with specific Squarespace themes but should work anywhere. 
 
-Specifically, its goal is to smoothly embed a full-size, auto-playing video upon a click of the target image, and then replace the embed with some HTML or the original image when playback is complete, leading to a much smoother experience for the listener. 
+Specifically, its goal is to smoothly embed a full-size, auto-playing video upon a click of the target image, and then take an action when the playing is done, like replace the embed with some HTML or the original image, leading to a much smoother experience for the watcher. 
 
-Example: https://restlesslabs.squarespace.com (scroll down a bit)
+Example: https://theplanaproj.com
 
 This script is designed to work with a few squarespace themes (Squarespace uses YUI3); it's been tested on Bedford and Marquee, which both have large full-sized images but mediocre vimeo playback options. 
 
 This script targets a specific div containing an image (such as Marquee's parallax-item divs with named data-url-id properties). It waits for the DOM to finish loading and then overlays a "play" button and make the entire div clickable. It then sets up a click callback that inserts the embed iframe and sets up Vimeo's 'finish' callback. Upon finishing, it takes more html (e.g. a return to the original image or a call to action) and inserts it back into place of the iframe. 
-
-The variables are: 
- - the vimeo video URL
- - the target div
- - the url of the play button (sample included in file)
